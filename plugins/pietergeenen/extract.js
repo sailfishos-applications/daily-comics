b9b042e76501776261d316270fd91a9706922c16
olf <Olf0@users.noreply.github.com>
2023-10-01 21:06:49 +0200

    [pietergeenen/extract.js] Improve RegEx

--- plugins/pietergeenen/extract.js
+++ plugins/pietergeenen/extract.js
@@ -1,7 +1,6 @@
 function(page) {
-//    var regex = /<img\ssrc="([^"]*)"/;
-    var regex = /<source\ssrcset="([^"]*)"\stype="image\/jpeg">/;
+    var regex = /<source[^>]*srcset="([^"]*)"[^>]*type="image\/jpeg">/;
     var match = regex.exec(page);
-    return match[1].replace('&amp;', '&');
-//    return match[1].replace('\/thumbs', '');
+    // return match[1].replace('&amp;', '&');
+    return match[1]
 }
