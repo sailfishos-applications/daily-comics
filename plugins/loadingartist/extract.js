e2854e2858f02d4f166e8bfb6b9f0e2556076fd9
olf <Olf0@users.noreply.github.com>
2023-10-01 23:14:17 +0200

    [loadingartist/extract.js] Document absolute path without server prefix

--- plugins/loadingartist/extract.js
+++ plugins/loadingartist/extract.js
@@ -1,5 +1,6 @@
 function(page) {
     var regex = /<img[^>]+src="(\/comic[^"]+\\.jpg)"/;
     var match = regex.exec(page);
-    return "https://loadingartist.com" + match[1];
+    return match[1];
+    // return "https://loadingartist.com" + match[1];
 }
