f81bf9f2af4ca3bcc7e7ff1aaac87b0f605e1e9b
olf <Olf0@users.noreply.github.com>
2023-10-01 22:12:41 +0200

    [lectrr/extract.js} Improve RegEx

--- plugins/lectrr/extract.js
+++ plugins/lectrr/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img src="(.+\\.jpg)((?!<img).)*artoon van de dag/;
+    var regex = /<img src="([^"]+\\.jpg)((?!<img).)*artoon van de dag/;
     var match = regex.exec(page);
     return match[1];
 }
