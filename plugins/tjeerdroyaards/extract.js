0c73d7ce4cb790e382724775d44681203428e45c
olf <Olf0@users.noreply.github.com>
2023-10-01 23:12:17 +0200

    [tjeerdroyaards/extract.js] Improve RegEx

--- plugins/tjeerdroyaards/extract.js
+++ plugins/tjeerdroyaards/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img[^>]*src="([^"]*\/\\.a\/[^"]*)"/;
+    var regex = /<img[^>]+src="([^"]*\/\\.a\/[^"]+)"/;
     var match = regex.exec(page);
     return match[1];
 }
