2299d8e24ebd8af03afee1b37b4eea761b672f2d
olf <Olf0@users.noreply.github.com>
2023-10-01 22:10:15 +0200

    [lectrr/extract.js] Fix extraction RegEx

--- plugins/lectrr/extract.js
+++ plugins/lectrr/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img src="(.+?jpg)\?.+?alt="Cartoon\svan\sde\sdag/;
+    var regex = /<img src="(.+\\.jpg)((?!<img).)*artoon van de dag/;
     var match = regex.exec(page);
     return match[1];
 }
