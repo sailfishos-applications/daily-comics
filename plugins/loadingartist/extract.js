4651541e5ebdc86e3b4192832147266583f3d97d
olf <Olf0@users.noreply.github.com>
2023-10-02 00:10:35 +0200

    Add 7 comic strips: Gorilla, Hein de Kort, Lectrr, De wereld v. A. D., Schot, Tjeerd Royards, ZAK (#4)
    * New cartoon: Lectrr
      Since Clay Bennett and der Spiegel Cartoon des Tages could enter the plugins with their political cartoons, I thought: why not Lectrr?
    
    * Added plugin: Tjeerd Royaards
      Let's try again to get Tjeerd Royaards in, since political cartoons no longer seem to be banned. Supplying a large cover image is kind of difficult. Could we do it like this?
    
    * New cover jpgs
      Why not?
    
    * New cartoon: Hein de Kort
      Legendary cartoonist
    
    * New cartoon: Zak
      Flemish cartoonist, unfortunately old cartoons
    
    * 3 new cartoons
      Can't get enough!
    
    * Swamp and Loading Artist
      Small changes
    
    * [tjeerdroyaards/info.json] Shorten name
    
    * [tjeerdroyaards/info.json] Do not use whiote as bg colour, but bg colour if the author's scetch
    
    * [tjeerdroyaards/info.json] Use About page instead of https://tjeerdroyaards..com/
    
    * [tjeerdroyaards/extract.js] Rectify RegEx
    
    * [swamp/info.json] Improve weblinks
    
    * [shot/info.json] Improve link, correct langauage and move to basvanderschot
    
    * [basvanderschot/extract.js] Rename directory & improve RegEx
    
    * [basvanderschot/example.jpg] Add new example image
    
    * [pietergeenen/extract.js] Improve RegEx
    
    * [pietergeenen/info.json] Alter homepage
    
    * [lectrr/info.json] HTTPS Hompage
    
    * [lectrr/extract.js] Fix extraction RegEx
    
    * [lectrr/extract.js} Improve RegEx
    
    * [lectrr/info.json] Align background colour
    
    * [loadingartist/extract.js] Fix Regex and return value
    
    * [loadingartist/info.json] Use HTTPS
    
    * [gorilla/extract.js] Fix RegEx
    
    * [gorilla/info.json] Fix authors
    
    * [zak/extract.js] Enhance
    
    * [zak/extract.js] Improve RegEx
    
    * [tjeerdroyaards/extract.js] Improve RegEx
    
    * [loadingartist/extract.js] Document absolute path without server prefix
    
    * [heindekort/info.json] Rectify link
    
    * [heindekort/extract.js] Improve RegEx
    
    * [plugins] Added `gorilla/example.jpg` and moved `schot`

--- plugins/loadingartist/extract.js
+++ plugins/loadingartist/extract.js
@@ -1,5 +1,6 @@
 function(page) {
-    var regex = /<img[^>]*src="([^"]*\/wp-content\/uploads\/[^"]*)"/;
+    var regex = /<img[^>]+src="(\/comic[^"]+\\.jpg)"/;
     var match = regex.exec(page);
     return match[1];
+    // return "https://loadingartist.com" + match[1];
 }
