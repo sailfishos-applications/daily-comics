20800d3d745b652d6b3d7353c4df659022e0f507
olf <Olf0@users.noreply.github.com>
2023-10-09 06:59:32 +0200

    [generate_list.sh] Optimise slightly

--- resources/generate_list.sh
+++ resources/generate_list.sh
@@ -5,8 +5,8 @@ PREFIX_LIST="The |La |Le |Les |L['’]|De |Het |Die |Der |Das |El |Los |Las |Lo 
 # Retrieve the real names of the plugins,
 # order them ignoring some prefixes and
 # prepend a dash ("-") to each line
-grep '"name"' "${SCRIPT_DIR}"/../plugins/*/info.json \
-  | sed -E -e 's/.*"name": "([^"]*)".*/\1/' -e "s/^($PREFIX_LIST)(.*)/\\2,\\1/" \
+grep '"name":' "${SCRIPT_DIR}"/../plugins/*/info.json \
+  | sed -E -e 's/.*"name":\s*"([^"]*)".*/\1/' -e "s/^($PREFIX_LIST)(.*)/\\2,\\1/" \
   | sort \
   | sed -E -e "s/^(.*),($PREFIX_LIST)\$/\\2\\1/" -e 's/^/- /' \
   > "${SCRIPT_DIR}"/../info/comics_list.md
