bc73088fda5a64ce8562900da060393839390f92
olf <Olf0@users.noreply.github.com>
2023-10-09 21:34:58 +0200

    [generate_list.sh] Normalise variable names

--- resources/generate_list.sh
+++ resources/generate_list.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(dirname "$(readlink -e "$0")")
-PREFIX_LIST="The |La |Le |Les |L['’]|El |Los |Las |Lo |Il |I |Gli |Una |Uno |Une |De |Het |Die |Der |Das "
+script_dir="$(dirname "$(readlink -e "$0")")"
+prefix_list="The |La |Le |Les |L['’]|El |Los |Las |Lo |Il |I |Gli |Una |Uno |Une |De |Het |Die |Der |Das "
 # Retrieve the real names of the plugins,
 # order them ignoring some prefixes and
 # prepend a dash ("-") to each line:
-grep -Fhm 1 '"name":' "${SCRIPT_DIR}"/../plugins/*/info.json \
-  | sed -E -e 's/.*"name":\s*"([^"]*)".*/\1/' -e "s/^($PREFIX_LIST)(.*)/\\2,\\1/" \
+grep -Fhm 1 '"name":' "$script_dir"/../plugins/*/info.json \
+  | sed -E -e 's/^.*"name":\s*"([^"]*)".*$/\1/' -e "s/^($prefix_list)(.*)\$/\\2,\\1/" \
   | sort \
-  | sed -E -e "s/^(.*),($PREFIX_LIST)\$/\\2\\1/" -e 's/^/- /' \
-  > "${SCRIPT_DIR}"/../info/comics_list.md
+  | sed -E -e "s/^(.*),($prefix_list)\$/\\2\\1/" -e 's/^/- /' \
+  > "$script_dir"/../info/comics_list.md
 
