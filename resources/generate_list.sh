ab628de525353c04b35ffd967b1876af3fd109d9
olf <Olf0@users.noreply.github.com>
2023-10-09 06:15:47 +0200

    [generate_list.sh] Overhaul

--- resources/generate_list.sh
+++ resources/generate_list.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-SCRIPT_DIR=$( dirname "$( readlink --canonicalize "$0" )" )
-
-# retrieve the plugins name and order them
-# ignoring some prefixes
-grep -r '"name"' "${SCRIPT_DIR}"/../plugins/ \
-  | sed -r 's/.*"name": "([^"]*)".*/\1/' \
-  | sed -r 's/^(The|Le|Une) (.*)/\2, \1/' \
+SCRIPT_DIR=$( dirname "$( readlink -e "$0" )" )
+PREFIX_LIST="The |La |Le |Les |L['’]|De |Het |Die |Der |Das |El |Los |Las |Lo |Il |I |Gli |Una |Uno |Une "
+# Retrieve the real names of the plugins,
+# order them ignoring some prefixes and
+# prepend a dash ("-") to each line
+grep '"name"' "${SCRIPT_DIR}"/../plugins/*/info.json \
+  | sed -E -e 's/.*"name": "([^"]*)".*/\1/' -e "s/^($PREFIX_LIST)(.*)/\\2,\\1/" \
   | sort \
-  | sed -r 's/(.*), (The|Le|Une)$/\2 \1/' \
-  > "${SCRIPT_DIR}/../info/comics_list.md"
+  | sed -E -e "s/^(.*),($PREFIX_LIST)\$/\\2\\1/" -e 's/^/- /' \
+  > "${SCRIPT_DIR}"/../info/comics_list.md
 
-# prepend - to each line to create the list
-sed -i -e 's/^/- /' "${SCRIPT_DIR}/../info/comics_list.md"
