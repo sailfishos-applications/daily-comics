777f34ba1e34daff93453f5401d14b74155dba81
olf <Olf0@users.noreply.github.com>
2024-03-25 04:45:04 +0100

    [plugins/pietergeenen] Remove stale "De wereld van Anton Dingeman" (Pieter Geenen) (#29)

--- plugins/pietergeenen/extract.js
+++
@@ -1,6 +0,0 @@
-function(page) {
-    var regex = /<source[^>]*srcset="([^"]*)"[^>]*type="image\/jpeg">/;
-    var match = regex.exec(page);
-    // return match[1].replace('&amp;', '&');
-    return match[1]
-}
