b1497c9adf27977849304bbf10ca560a6fc05ab7
olf <Olf0@users.noreply.github.com>
2024-03-28 23:13:36 +0100

    Fix `loadingartist/extract.js` again, supersedes #72 (#73)

--- plugins/loadingartist/extract.js
+++ plugins/loadingartist/extract.js
@@ -1,6 +1,5 @@
 function(page) {
     var regex = /<img[^>]+src="*(\/comic\/[^ "]+)/;
     var match = regex.exec(page);
-    return match[2];
-    // return "https://loadingartist.com" + match[1];
+    return "https://loadingartist.com" + match[2];
 }
