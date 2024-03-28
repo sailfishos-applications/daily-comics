b7643c1728ad5d9ea4aa230b6e17b54aab5ad3e2
olf <Olf0@users.noreply.github.com>
2024-03-28 22:58:44 +0100

    Fix `loadingartist/extract.js`

--- plugins/loadingartist/extract.js
+++ plugins/loadingartist/extract.js
@@ -1,6 +1,6 @@
 function(page) {
-    var regex = /<img[^>]+src="(\/comic[^"]+\\.jpg)"/;
+    var regex = /<img[^>]+src="*(\/comic\/[^ "]+)/;
     var match = regex.exec(page);
-    return match[1];
+    return match[2];
     // return "https://loadingartist.com" + match[1];
 }
