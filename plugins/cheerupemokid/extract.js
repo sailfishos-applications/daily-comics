0fc71e062e88d7c39cfbead6954e28868c12f72f
olf <Olf0@users.noreply.github.com>
2024-03-30 00:25:52 +0100

    [plugins/cheerupemokid/extract.js] Fix RegEx

--- plugins/cheerupemokid/extract.js
+++ plugins/cheerupemokid/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img[^>]*src="([^"]*\/wp-content\/uploads\/[^"]*)"/;
+    var regex = /<img[^>]*src="(https://assets.amuniversal.com/[^"]*)"/;
     var match = regex.exec(page);
     return match[1];
 }
