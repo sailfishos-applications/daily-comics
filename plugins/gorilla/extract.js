1074c75e8099589f4859b9a11407e1a9c3735667
olf <Olf0@users.noreply.github.com>
2024-03-31 22:22:37 +0200

    Delete `plugins/gorilla`, because publishing new comics ceased (#91)

--- plugins/gorilla/extract.js
+++
@@ -1,6 +0,0 @@
-function(page) {
-    var regex = /<a[^>]+href="[^"]*\/cartoons\/.+<img[^>]+src="([^"]*)"/;
-    var match = regex.exec(page);
-    return match[1];
-    // return "https://www.groene.nl" + match[1];
-}
