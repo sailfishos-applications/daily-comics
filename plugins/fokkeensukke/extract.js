065485d72836d67325a39137ecf569d890aeedba
olf <Olf0@users.noreply.github.com>
2024-10-11 01:38:28 +0200

    [plugins/fokkeensukke/extract.js] Slightly enhance RegEx

--- plugins/fokkeensukke/extract.js
+++ plugins/fokkeensukke/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /\s+src="(https://images.nrc.nl/[^"]*)"/;
+    var regex = /\s+src="(https://images.nrc.nl/[^"]+)"/;
     var match = regex.exec(page);
     return match[1];
 }
