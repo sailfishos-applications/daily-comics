cf8e1a62283b9c6212ee2f694dda347881f5ab77
Otto J. Makela <extechop@gmail.com>
2025-01-28 16:10:54 +0200

    corrected extract to get rid of static string

--- plugins/kevinandkell/extract.js
+++ plugins/kevinandkell/extract.js
@@ -1,5 +1,5 @@
 function(page) {
     var regex = /<img\s+src="\/([^ "]+\.jpg)"\s+alt="Comic Strip"/;
     var match = regex.exec(page);
-    return "https://www.kevinandkell.com/" + match[1];
+    return match[1];
 }
