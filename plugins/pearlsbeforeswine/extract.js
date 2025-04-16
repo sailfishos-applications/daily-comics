78da5705ecae1dd01843c5d450334363c2ad0f5e
Frank Fischer <frank-fischer@shadow-soft.de>
2025-04-16 02:45:16 +0200

    Fix strips hosted at GoComics (#125)
    1. always use https without trailing slash
    2. new extract regexp
    
    Fixes issues #115, #118, #119, #121, #122 and addresses most of issue #123 (except for the strips added by PR #111).

--- plugins/pearlsbeforeswine/extract.js
+++ plugins/pearlsbeforeswine/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<picture class="[^"]*cropped-strip[^<]*<img[^>]* src="([^"]*)"/;
+    var regex = /imageSrcSet="(http[^" ?]+)/;
     var match = regex.exec(page);
     return match[1];
 }
