40586b161fc835fb16db3dcf6a489a30030f9f4f
olf <Olf0@users.noreply.github.com>
2025-07-10 20:40:35 +0200

    [plugins/sigmund/extract.js] Fix Sigmund

--- plugins/sigmund/extract.js
+++ plugins/sigmund/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img[^>]*src=(strips\/[^ ]*) /;
+    var regex = /<img id=\"strook[^>]*src="(http.*gif)"/;
     var match = regex.exec(page);
     return match[1];
 }
