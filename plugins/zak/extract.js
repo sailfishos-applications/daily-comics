8b3b767fa098c18d464587a835e9deb27211e4d1
olf <Olf0@users.noreply.github.com>
2025-07-14 04:12:54 +0200

    [plugins/zak/extract.js] Try to fix ZAK
    Still not working, out of ideas why.

--- plugins/zak/extract.js
+++ plugins/zak/extract.js
@@ -1,6 +1,7 @@
 function(page) {
     var regex = /&quot;path&quot;:&quot;(\S+ - Zak - De Morgen\.\S+)&quot;/;
     var match = regex.exec(page);
-    return "https://admin.presscartoondev.eu/" + match[1].replace(/\\\//g, '/');
+    var path1 = match[1].replace(/\\\//g, '/');
+    return "https://admin.presscartoondev.eu/" + path1.replace(/ /g, '%20');
     // return match[1].replace(/&amp;/g, '&');
 }
