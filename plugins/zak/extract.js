0b0c9f3bed7786ad99c3c39fb79369cdafe8fb1b
olf <Olf0@users.noreply.github.com>
2025-07-14 03:30:53 +0200

    [plugins/zak/] Adapt to new website (#202)
    * [plugins/zak/extract.js] Adapt to new website
    
    * [plugins/zak/info.json] Adapt to new website
    
    * [plugins/zak/extract.js] Fix "copy'o"

--- plugins/zak/extract.js
+++ plugins/zak/extract.js
@@ -1,6 +1,6 @@
 function(page) {
-    var regex = /<meta[^>]+property="og:image"[^>]+content="([^"]+)"/;
+    var regex = /&quot;path&quot;:&quot;(\S+ - Zak - De Morgen\.\S+)&quot;/;
     var match = regex.exec(page);
-    return match[1];
+    return "https://admin.presscartoondev.eu/" + match[1].replace(/\\\//g, '/');
     // return match[1].replace(/&amp;/g, '&');
 }
