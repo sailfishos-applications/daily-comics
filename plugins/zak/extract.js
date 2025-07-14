0b626300e8ed22a5dd5e91450040074f12463524
olf <Olf0@users.noreply.github.com>
2025-07-14 03:28:18 +0200

    Update extract.js

--- plugins/zak/extract.js
+++ plugins/zak/extract.js
@@ -1,6 +1,5 @@
 function(page) {
-    var regex = /&quot;path&quot;:&quot;(\S+ - Zak - De Morgen\.\S+)&quot;/
-        <meta[^>]+property="og:image"[^>]+content="([^"]+)"/;
+    var regex = /&quot;path&quot;:&quot;(\S+ - Zak - De Morgen\.\S+)&quot;/;
     var match = regex.exec(page);
     return "https://admin.presscartoondev.eu/" + match[1].replace(/\\\//g, '/');
     // return match[1].replace(/&amp;/g, '&');
