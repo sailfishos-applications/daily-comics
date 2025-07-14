fc97796f7c62e3605c8222aaa1138ba69ff84913
olf <Olf0@users.noreply.github.com>
2025-07-14 02:48:23 +0200

    [plugins/lectrr/] Adapt to altered website (#201)
    * [plugins/lectrr/info.json] Adapt to altered website
    
    * [plugins/lectrr/extract.js] Adapt to altered website

--- plugins/lectrr/extract.js
+++ plugins/lectrr/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /data-vr-contentbox="Lectrr, cartoon van de dag".+?<img[^>]+src="([^"]+.jpg)".+?<span>Lectrr, cartoon van de dag<\/span>/;
+    var regex = /<img[^>]+src="(https:\/\/prod-img.standaard.be\/public\/nieuws\/\S+)/;
     var match = regex.exec(page);
     return match[1];
 }
