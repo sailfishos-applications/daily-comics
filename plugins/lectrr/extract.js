483c5ae53b85a0e91d1c8769de988772b32331f0
Rob K <github@cow-n-berg.nl>
2025-08-15 17:03:26 +0200

    Updated: lectrr
    The info.json needs a fresh stripSource url monthly (unfortunately)

--- plugins/lectrr/extract.js
+++ plugins/lectrr/extract.js
@@ -1,5 +1,6 @@
-function(page) {
-    var regex = /<img[^>]+src="(https:\/\/prod-img.standaard.be\/public\/nieuws\/\S+)/;
+function(page) { 
+    var regex = /(https:[^"]+?[lectrr|vakantie][^"]+?\/BASE_WIDTH\/[^"]+?\.jpg)"/g;
     var match = regex.exec(page);
+    match = regex.exec(page);
     return match[1];
 }
