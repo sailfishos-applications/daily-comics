ce6bf85a1babdd6367dd535af9819e33555ad96c
Otto J. Makela <extechop@gmail.com>
2025-10-21 13:46:43 +0300

    Make Helsingin Sanomat strips work again

--- plugins/lassijaleevi/extract.js
+++ plugins/lassijaleevi/extract.js
@@ -1,5 +1,7 @@
 function(page) {
-    var regex = /<img[^>]+srcset="(\/\/[^ "]+)/;
+    // Thanks to Matti K. for giving me pointers to the api interface!
+    var regex = /"url":\s*"([^"]+)/;
     var match = regex.exec(page);
-    return "https:" + match[1];
+    var url = match[1];
+    return url.replace("WIDTH.EXT","1920.jpg");
 }
