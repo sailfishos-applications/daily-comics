2629790f5b6fd61ef9f11c5b476a2445043c03d5
Frank Paul Silye <frankps@gmail.com>
2026-04-14 22:54:19 +0200

    Add files via upload

---
+++ translations/harbour-dailycomics-nb_NO.ts
@@ -0,0 +1,331 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="63"/>
+        <source>Read your favourite comic strips every day.</source>
+        <translation>Les dine favorittegneserier hver dag.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="79"/>
+        <source>Version %1</source>
+        <translation>Versjon %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="92"/>
+        <source>&lt;a href=&quot;%1&quot;&gt;Source code repository&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;%1&quot;&gt;Kildekode-arkiv&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="107"/>
+        <source>License: %1</source>
+        <translation>Lisens: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="121"/>
+        <source>&lt;a href=&quot;%1&quot;&gt;Issue tracker for bug reports, feature suggestions and help requests&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;%1&quot;&gt;Issue tracker for feilrapporter, funksjonsforslag og hjelp&lt;/a&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ComicInfoPage</name>
+    <message>
+        <location filename="../qml/pages/ComicInfoPage.qml" line="46"/>
+        <source>Comic info</source>
+        <translation>Informasjon om tegneserien</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ComicInfoPage.qml" line="50"/>
+        <source>Authors</source>
+        <translation>Tegneserieskapere</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ComicInfoPage.qml" line="50"/>
+        <source>Author</source>
+        <translation>Tegneserieskaper</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ComicInfoPage.qml" line="55"/>
+        <source>Language</source>
+        <translation>Språk</translation>
+    </message>
+</context>
+<context>
+    <name>ComicInfoPanel</name>
+    <message>
+        <location filename="../qml/components/ComicInfoPanel.qml" line="63"/>
+        <source>Name</source>
+        <translation>Navn</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicInfoPanel.qml" line="81"/>
+        <source>Authors</source>
+        <translation>Tegneserieskapere</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicInfoPanel.qml" line="81"/>
+        <source>Author</source>
+        <translation>Tegneserieskaper</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicInfoPanel.qml" line="98"/>
+        <source>Language</source>
+        <translation>Språk</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicInfoPanel.qml" line="116"/>
+        <source>Example</source>
+        <translation>Eksempel</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicInfoPanel.qml" line="151"/>
+        <source>Go to homepage</source>
+        <translation>Gå til hjemmeside</translation>
+    </message>
+</context>
+<context>
+    <name>ComicItem</name>
+    <message>
+        <location filename="../qml/components/ComicItem.qml" line="29"/>
+        <source>Loading comic</source>
+        <translation>Laster tegneserie</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicItem.qml" line="30"/>
+        <source>Cannot display comic</source>
+        <translation>Kan ikke vise tegneserie</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicItem.qml" line="31"/>
+        <source>Cannot download comic</source>
+        <translation>Kan ikke laste ned tegneserie</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicItem.qml" line="32"/>
+        <source>Cannot extract comic</source>
+        <translation>Kan ikke pakke ut tegneserie</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicItem.qml" line="33"/>
+        <source>Cannot save comic</source>
+        <translation>Kan ikke lagre tegneserie</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ComicItem.qml" line="68"/>
+        <source>Please report if this issue persists.</source>
+        <translation>Vennligst rapporter hvis problemet vedvarer.</translation>
+    </message>
+</context>
+<context>
+    <name>ComicsSettingsPage</name>
+    <message>
+        <location filename="../qml/pages/ComicsSettingsPage.qml" line="35"/>
+        <source>Save</source>
+        <translation>Lagre</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ComicsSettingsPage.qml" line="36"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ComicsSettingsPage.qml" line="54"/>
+        <source>Clear all</source>
+        <translation>Tøm alle</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ComicsSettingsPage.qml" line="60"/>
+        <source>Select all</source>
+        <translation>Velg alle</translation>
+    </message>
+</context>
+<context>
+    <name>DefaultCover</name>
+    <message numerus="yes">
+        <location filename="../qml/cover/DefaultCover.qml" line="44"/>
+        <source>%n new comic(s)</source>
+        <translation>
+            <numerusform>%n ny tegneserie</numerusform>
+            <numerusform>%n nye tegneserier</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>EndPanel</name>
+    <message>
+        <location filename="../qml/components/EndPanel.qml" line="36"/>
+        <source>That&apos;s all for now!</source>
+        <translation>Det var alt for nå!</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/EndPanel.qml" line="55"/>
+        <source>If you like this app, please consider to support your favorite comic artists</source>
+        <translation>Hvis du liker denne appen, vurder å støtte dine favoritt tegneserieskapere</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/EndPanel.qml" line="75"/>
+        <source>Return to favorites</source>
+        <translation>Gå tilbake til favoritter</translation>
+    </message>
+</context>
+<context>
+    <name>FavoritesPage</name>
+    <message>
+        <location filename="../qml/pages/FavoritesPage.qml" line="70"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FavoritesPage.qml" line="74"/>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FavoritesPage.qml" line="78"/>
+        <source>Read all new comics</source>
+        <translation>Les alle nye tegneserier</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FavoritesPage.qml" line="78"/>
+        <source>No new comic</source>
+        <translation>Ingen nye tegneserier</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FavoritesPage.qml" line="87"/>
+        <source>No comic selected</source>
+        <translation>Ingen tegneserier valgt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FavoritesPage.qml" line="88"/>
+        <source>Choose your favorite comics in the settings page</source>
+        <translation>Velg dine favoritt-tegneserier på innstillingssiden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FavoritesPage.qml" line="108"/>
+        <source>Remove from favorites</source>
+        <translation>Fjern fra favoritter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FavoritesPage.qml" line="128"/>
+        <source>Fetching comic strips</source>
+        <translation>Henter tegneserier</translation>
+    </message>
+</context>
+<context>
+    <name>ImageOverlay</name>
+    <message>
+        <location filename="../qml/components/ImageOverlay.qml" line="80"/>
+        <source>Comic strip was saved to gallery</source>
+        <translation>Tegneserien ble lagret i Galleri</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ImageOverlay.qml" line="81"/>
+        <source>Unable to save comic strip to gallery</source>
+        <translation>Kunne ikke lagre tegneserien i Galleri</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ImageOverlay.qml" line="93"/>
+        <source>Comic strip URL copied to clipboard</source>
+        <translation>Nettadressen til tegneserien ble kopiert til utklippstavlen</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ImageOverlay.qml" line="94"/>
+        <source>Unable to copy comic strip URL to clipboard</source>
+        <translation>Kunne ikke kopiere nettadressen til utklippstavlen</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressInfoBar</name>
+    <message numerus="yes">
+        <location filename="../qml/utils/ProgressInfoBar.qml" line="72"/>
+        <source>%n New</source>
+        <translation>
+            <numerusform>%n ny</numerusform>
+            <numerusform>%n nye</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/utils/ProgressInfoBar.qml" line="99"/>
+        <source>%n Error(s)</source>
+        <translation>
+            <numerusform>%n feil</numerusform>
+            <numerusform>%n feil</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>ReportNewComicRectangle</name>
+    <message>
+        <location filename="../qml/components/ReportNewComicRectangle.qml" line="42"/>
+        <source>Please report if you like to see some additional comic(s)</source>
+        <translation>Gi beskjed hvis du ønsker å se flere tegneserier</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsInfoHint</name>
+    <message>
+        <location filename="../qml/components/SettingsInfoHint.qml" line="23"/>
+        <source>Hold cover to display comic info</source>
+        <translation>Hold inne omslaget for å vise informasjon</translation>
+    </message>
+</context>
+<context>
+    <name>ShareLinkPage</name>
+    <message>
+        <location filename="../qml/pages/ShareLinkPage.qml" line="25"/>
+        <source>Share comics link</source>
+        <translation>Del lenke til tegneserien</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ShareLinkPage.qml" line="36"/>
+        <source>No sharing accounts available.  You can add accounts in the SailfishOS settings.</source>
+        <translation>Ingen kontoer for deling er tilgjengelige. Du kan legge til kontoer i SailfishOS-innstillingene.</translation>
+    </message>
+</context>
+<context>
+    <name>SlideshowFlickHint</name>
+    <message>
+        <location filename="../qml/components/SlideshowFlickHint.qml" line="27"/>
+        <source>Flick to display next comic</source>
+        <translation>Sveip for å se neste tegneserie</translation>
+    </message>
+</context>
+<context>
+    <name>StatusOverlay</name>
+    <message>
+        <location filename="../qml/utils/StatusOverlay.qml" line="24"/>
+        <source>An error occured</source>
+        <translation>Det oppstod en feil</translation>
+    </message>
+    <message>
+        <location filename="../qml/utils/StatusOverlay.qml" line="91"/>
+        <source>Network error</source>
+        <translation>Nettverksfeil</translation>
+    </message>
+    <message>
+        <location filename="../qml/utils/StatusOverlay.qml" line="92"/>
+        <source>Parsing error</source>
+        <translation>Tolkingsfeil</translation>
+    </message>
+    <message>
+        <location filename="../qml/utils/StatusOverlay.qml" line="93"/>
+        <source>Saving error</source>
+        <translation>Lagringsfeil</translation>
+    </message>
+</context>
+<context>
+    <name>ZoomableImage</name>
+    <message>
+        <location filename="../qml/components/ZoomableImage.qml" line="157"/>
+        <source>Image error</source>
+        <translation>Bildefeil</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ZoomableImage.qml" line="157"/>
+        <source>Cannot display strip</source>
+        <translation>Kan ikke vise tegneseriestripen</translation>
+    </message>
+</context>
+</TS>
